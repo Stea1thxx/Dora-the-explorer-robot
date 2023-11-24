064a7940df9b8044f10e3cc967027741972334b4
Stea1thxx <87929377+Stea1thxx@users.noreply.github.com>
2023-11-24 16:43:51 +0000

    Update Joystick Code
    New code to make the servos work

--- Arduino Code/NewJoystickCode/NewJoystickCode.ino
+++ Arduino Code/NewJoystickCode/NewJoystickCode.ino
@@ -1,44 +1,39 @@
+//INITIALISING VARIABLES
 #include <Servo.h>
-
+Servo servoLeft;                        // Declare left and right servos
+Servo servoRight;
 int value;
+int servoLeftPW;
+int servoRightPW;
 bool reverse;
 
-//Map 0-1023 joystick value to 0-254. hence 127 is the center value which we send.
-//It also adjust the deadband in joystick as it's center value is not always 511. It is little different.
-//So we need to add some deadband to center value. in our case 500-530. Any value in this deadband range is mapped to center 127.
-//Jotstick values range from 0-1023. 
-int mapAndAdjustJoystickDeadBandValues(int value, bool reverse)
-{
-  if (value >= 530)
-  {
-    value = map(value, 530, 1023, 127, 254);
-  }
-  else if (value <= 500)
-  {
-    value = map(value, 500, 0, 127, 0);  
-  }
-  else
-  {
-    value = 127;
-  }
-
-  if (reverse)
-  {
-    value = 254 - value;
-  }
-  return value;
-}
-
 void setup(){
-  
   Serial.begin(9600);
   Serial.println("SETUP------------------");
+  servoLeft.attach(9);                
+  servoRight.attach(10);
 }
 
 void loop()
 {
-  value = analogRead(A0);
-  Serial.println(mapAndAdjustJoystickDeadBandValues(value,reverse));
-  delay(500);
-}
+  value = analogRead(A0); //UP AND DOWN, (VRY) - Yellow wire
 
+  //Maping JS to Servos
+  if (value >= 530)
+  {
+    servoLeftPW = map(value, 530, 1023, 1500, 1700);
+    servoRightPW = map(value, 530, 1023, 1500, 1300);
+  }
+  else
+  {
+    //Stays still
+    servoLeftPW = 1500;
+    servoRightPW = 1500;
+  }
+
+  servoLeft.writeMicroseconds(servoLeftPW);    // Left wheel counterclockwise
+  servoRight.writeMicroseconds(servoRightPW);   // Right wheel clockwise
+  Serial.println("Left: " + String(servoLeftPW));
+  Serial.println("Right: " + String(servoRightPW));
+  //delay(500);
+}
