2d720948ec99d083745a09a6693f0922b2133bdb
Zinxiee <zachdanzig@gmail.com>
2023-12-01 15:20:46 +0000

    Bluetooth test code added

--- Arduino Code/Wireless/simple_transmit/simple_transmit.ino
+++ Arduino Code/Wireless/simple_transmit/simple_transmit.ino
@@ -3,19 +3,23 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(7, 8); // CE, CSN
+RF24 radio(7, 8, 4000000); // CE, CSN
 
-const byte address[6] = "00001";
+const byte address[1] = "00001";
 
 void setup() {
+  Serial.begin(9600);
   radio.begin();
+  radio.setAutoAck(true);
   radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MAX);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.setDataRate(RF24_250KBPS);
   radio.stopListening();
 }
 
 void loop() {
   const char text[] = "Hello World";
   radio.write(&text, sizeof(text));
+  Serial.println("SENT");
   delay(1000);
 }
